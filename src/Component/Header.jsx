846307db02c7bf1fe65d20623acff719dae0bfb4
PatilKishori <kishoripatil2929@gmail.com>
2022-08-27 14:46:56 +0530

    updated

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
     <li className="nav-item active">
-        <Link to="" className="nav-link">Header</Link>
+        <Link to="" className="nav-link">Main Header</Link>
       </li>
       <li className="nav-item active">
         <Link to="/registration" className="nav-link">Registration</Link>
